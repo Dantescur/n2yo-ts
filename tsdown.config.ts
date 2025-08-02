cfa0f277ebb8c04bb30862954dd3c3d3acad01ef
= <daniel@thedaniweb.eu.org>
2025-08-02 18:20:37 -0400

    build: support cjs

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,15 +2,15 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
+  entry: 'src/index.ts',
   platform: 'neutral',
   target: 'es2022',
+  format: ['es', 'cjs'],
   pub: true,
   outDir: './dist',
   dts: true,
   exports: true,
   shims: true,
-  format: ['esm'],
   report: true,
 })
 /* v8 ignore stop */
