7273a9a93432957bf965a0cce5b2defc5c3e1a20
= <daniel@thedaniweb.eu.org>
2025-08-01 22:41:12 -0400

    docs: enhanced jsdocs for index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Re-export main client and all public types/utilities
 import { N2YOClient } from './client'
 export { InvalidParameterError, N2YOError, RateLimitError } from './errors'
 export {
@@ -13,6 +14,14 @@ export {
 
 export { getAllCategories, splitTle, timestampToDate } from './helpers'
 
-// Factory function with explicit return type
+/**
+ * Factory function for creating N2YOClient instances.
+ *
+ * @param apiKey - Your N2YO.com API key
+ * @returns A new {N2YOClient} instance
+ *
+ * @example
+ * const client = createN2YOClient('your-api-key-here')
+ */
 export const createN2YOClient = (apiKey: string): N2YOClient =>
   new N2YOClient(apiKey)
